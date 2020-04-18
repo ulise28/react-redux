d2731c2863efdc463c0b86e9598a44b49c48877d
ulise28 <ulise28@gmail.com>
2020-04-19 01:16:35 +0200

    Added redux pipeline

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <div>Courses list</div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h5>Add Course</h5>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Submit Course" />
+      </form>
+    );
   }
 }
+
 export default CoursesPage;
